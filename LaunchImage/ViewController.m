f235d7207d5f452be39405fc9269a13b5cf2f7c5
Noah <noah.gao@zoom.us>
2020-10-20 15:16:01 +0800

    测试2： 添加启动图，图片尺寸为 2400 * 2400, 模拟器可以运行，并生成文件，真机概率性的黑屏，横屏情况下，黑屏概率更高
    模拟器（iPad Pro 12.9）：
    1. 模拟器可以，沙盒生成了对应的图片 (横竖屏都行)
    
    真机：
    
    1. 沙盒生成了对应的启动图片，但是图片为黑屏图片
    2.
    
    1. 真机黑屏
    2. 测试沙盒未生成目录以及对应文件

--- LaunchImage/ViewController.m
+++ LaunchImage/ViewController.m
@@ -9,13 +9,78 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
 @end
 
 @implementation ViewController
 
+/// 系统启动图缓存路径
+- (NSString *)launchImageCacheDirectory {
+
+    NSString *bundleID = [NSBundle mainBundle].infoDictionary[@"CFBundleIdentifier"];
+    NSFileManager *fm = [NSFileManager defaultManager];
+
+    // iOS13之前
+    NSString *cachesDirectory = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
+    NSString *snapshotsPath = [[cachesDirectory stringByAppendingPathComponent:@"Snapshots"] stringByAppendingPathComponent:bundleID];
+    if ([fm fileExistsAtPath:snapshotsPath]) {
+        return snapshotsPath;
+    }
+
+    // iOS13
+    NSString *libraryDirectory = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) firstObject];
+    snapshotsPath = [NSString stringWithFormat:@"%@/SplashBoard/Snapshots/%@ - {DEFAULT GROUP}", libraryDirectory, bundleID];
+    if ([fm fileExistsAtPath:snapshotsPath]) {
+        return snapshotsPath;
+    }
+
+    return nil;
+}
+
+/// 系统缓存启动图后缀名
+- (BOOL)isSnapShotName:(NSString *)name {
+    // 新系统后缀
+    NSString *snapshotSuffixs = @".ktx";
+    if ([name hasSuffix:snapshotSuffixs]) {
+        return YES;
+    }
+    // 老系统后缀
+    snapshotSuffixs = @".png";
+    if ([name hasSuffix:snapshotSuffixs]) {
+        return YES;
+    }
+    return NO;
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSString *path= [self launchImageCacheDirectory];
+    NSLog(@"%@", path);
+
+    NSFileManager *fm = [NSFileManager defaultManager];
+    if ([fm fileExistsAtPath:path])
+    {
+        for (NSString *name in [fm contentsOfDirectoryAtPath:path error:nil])
+        {
+            if ([self isSnapShotName:name])
+            {
+                UIImage *image = [UIImage imageNamed:[path stringByAppendingPathComponent:name]];
+
+                if( image != nil )
+                {
+                    self.imageView.image = image;
+                    return;
+                }
+            }
+        }
+    }
+    else
+    {
+        NSLog(@" NOT exist ");
+    }
 }
 
 
