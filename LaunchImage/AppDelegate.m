6b31356071da835f47b2ec0dc9ff43097452c278
Noah <noah.gao@zoom.us>
2020-10-20 15:02:30 +0800

    测试1： 添加启动图，图片尺寸为 2732 * 2732,  模拟器与真机启动图均不行
    模拟器：
    1. 模拟器启动图黑屏
    2. 沙盒目录并未生成启动图片；
    
    真机：
    
    1. 真机黑屏
    2. 测试沙盒未生成目录以及对应文件

--- LaunchImage/AppDelegate.m
+++ LaunchImage/AppDelegate.m
@@ -13,9 +13,70 @@
 
 @implementation AppDelegate
 
+/// 系统启动图缓存路径
+- (NSString *)launchImageCacheDirectory {
+
+    NSString *bundleID = [NSBundle mainBundle].infoDictionary[@"CFBundleIdentifier"];
+    NSFileManager *fm = [NSFileManager defaultManager];
+
+    // iOS13之前
+    NSString *cachesDirectory = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
+    NSString *snapshotsPath = [[cachesDirectory stringByAppendingPathComponent:@"Snapshots"] stringByAppendingPathComponent:bundleID];
+    if ([fm fileExistsAtPath:snapshotsPath]) {
+        return snapshotsPath;
+    }
+
+    // iOS13
+    NSString *libraryDirectory = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) firstObject];
+    snapshotsPath = [NSString stringWithFormat:@"%@/SplashBoard/Snapshots/%@ - {DEFAULT GROUP}", libraryDirectory, bundleID];
+    if ([fm fileExistsAtPath:snapshotsPath]) {
+        return snapshotsPath;
+    }
+
+    return nil;
+}
+
+/// 系统缓存启动图后缀名
+- (BOOL)isSnapShotName:(NSString *)name {
+    // 新系统后缀
+    NSString *snapshotSuffixs = @".ktx";
+    if ([name hasSuffix:snapshotSuffixs]) {
+        return YES;
+    }
+    // 老系统后缀
+    snapshotSuffixs = @".png";
+    if ([name hasSuffix:snapshotSuffixs]) {
+        return YES;
+    }
+    return NO;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+    NSString *path= [self launchImageCacheDirectory];
+    NSLog(@"%@", path);
+
+    NSFileManager *fm = [NSFileManager defaultManager];
+    if ([fm fileExistsAtPath:path])
+    {
+
+        NSMutableArray *cacheImageNames = [NSMutableArray array];
+
+        for (NSString *name in [fm contentsOfDirectoryAtPath:path error:nil])
+        {
+            if ([self isSnapShotName:name])
+            {
+                NSLog(@"name = %@", name);
+                [cacheImageNames addObject:name];
+            }
+        }
+    }
+    else
+    {
+        NSLog(@" NOT exist ");
+    }
+
+
     return YES;
 }
 
